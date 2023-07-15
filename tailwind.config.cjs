d344a37d3bb35b1ee473923f36f870152fbe2271
Parinya T <pikcolokung@gmail.com>
2023-07-15 16:04:28 +0700

    chore: bump dependencies version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -68,7 +68,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/line-clamp"),
     require("daisyui"),
   ],
   daisyui: {
