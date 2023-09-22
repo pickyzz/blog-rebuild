898e769d8b96924d2a2b48f5d8e687317b7c2ef7
Parinya T. <pikcolokung@gmail.com>
2023-09-22 23:33:44 +0700

    chore: OG Image Reworks

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -1,8 +1,8 @@
 import satori, { type SatoriOptions } from "satori";
-import { SITE } from "@config";
-import { writeFile } from "node:fs/promises";
 import { Resvg } from "@resvg/resvg-js";
-import { getCollection } from "astro:content";
+import { type CollectionEntry } from "astro:content";
+import postOgImage from "@utils/og-templates/post";
+import siteOgImage from "@utils/og-templates/site";
 
 const fetchFonts = async () => {
   // Regular Font
@@ -21,100 +21,6 @@ const fetchFonts = async () => {
 };
 
 const { fontRegular, fontBold } = await fetchFonts();
-
-const ogImage = (text: string) => {
-  return (
-    <div
-      style={{
-        background: "#fefbfb",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          top: "-1px",
-          right: "-1px",
-          border: "4px solid #000",
-          background: "#ecebeb",
-          opacity: "0.9",
-          borderRadius: "4px",
-          display: "flex",
-          justifyContent: "center",
-          margin: "2.5rem",
-          width: "88%",
-          height: "80%",
-        }}
-      />
-
-      <div
-        style={{
-          border: "4px solid #000",
-          background: "#fefbfb",
-          borderRadius: "4px",
-          display: "flex",
-          justifyContent: "center",
-          margin: "2rem",
-          width: "88%",
-          height: "80%",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            margin: "20px",
-            width: "90%",
-            height: "90%",
-          }}
-        >
-          <p
-            style={{
-              fontSize: 72,
-              fontWeight: "bold",
-              maxHeight: "84%",
-              overflow: "hidden",
-            }}
-          >
-            {text}
-          </p>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-              marginBottom: "8px",
-              fontSize: 28,
-            }}
-          >
-            <span>
-              by{" "}
-              <span
-                style={{
-                  color: "transparent",
-                }}
-              >
-                "
-              </span>
-              <span style={{ overflow: "hidden", fontWeight: "bold" }}>
-                {SITE.author}
-              </span>
-            </span>
-
-            <span style={{ overflow: "hidden", fontWeight: "bold" }}>
-              {SITE.title}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const options: SatoriOptions = {
   width: 1200,
@@ -136,42 +42,18 @@ const options: SatoriOptions = {
   ],
 };
 
-const generateOgImages = async () => {
-  const postImportResult = await getCollection(
-    "blog",
-    ({ data }) => !data.draft
-  );
-  const posts = Object.values(postImportResult).filter(
-    ({ data }) => !data.ogImage
-  );
+function svgBufferToPngBuffer(svg: string) {
+  const resvg = new Resvg(svg);
+  const pngData = resvg.render();
+  return pngData.asPng();
+}
 
-  const imageGenerationPromises = posts.map(async post => {
-    const svg = await satori(ogImage(post.data.title), options);
+export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
+  const svg = await satori(postOgImage(post), options);
+  return svgBufferToPngBuffer(svg);
+}
 
-    // render png in production mode
-    if (import.meta.env.MODE === "production") {
-      const resvg = new Resvg(svg);
-      const pngData = resvg.render();
-      const pngBuffer = pngData.asPng();
-
-      const outputImage = `${post.slug}.png`;
-
-      const LIGHT_BLUE = "\x1b[94m";
-      const DARK_GRAY = "\x1b[30m";
-      const RESET = "\x1b[0m";
-      console.info(
-        `  ${LIGHT_BLUE}└─${RESET} output png image:`,
-        `${DARK_GRAY}/${outputImage}${RESET}`
-      );
-
-      await writeFile(`./dist/${outputImage}`, pngBuffer);
-    }
-  });
-
-  // Wait for all image generation operations to complete
-  await Promise.all(imageGenerationPromises);
-
-  return null;
-};
-
-export default generateOgImages;
+export async function generateOgImageForSite() {
+  const svg = await satori(siteOgImage(), options);
+  return svgBufferToPngBuffer(svg);
+}
