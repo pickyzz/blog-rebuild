b54093acf656ace2ccdfc1c553d549057977b99c
Parinya T. <pikcolokung@gmail.com>
2023-09-27 20:20:49 +0700

    chore: robot && pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from "@astrojs/react";
 import rehypeExternalLinks from "rehype-external-links";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
-import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
@@ -26,15 +25,6 @@ export default defineConfig({
       extendMarkdownConfig: true,
       smartypants: true,
       gfm: true,
-    }),
-    robotsTxt({
-      policy: [
-        {
-          userAgent: "*",
-          disallow: ["/search"],
-          crawlDelay: 10,
-        },
-      ],
     }),
   ],
   markdown: {
