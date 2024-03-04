7a6622089f3d96484fd3ae02bd80b8ef62f1065b
Parinya T. <pikcolokung@gmail.com>
2024-03-04 16:44:57 +0700

    fix: use modDatetime when more recent than pubDatetime only.

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -7,10 +7,18 @@ const getSortedPosts = (posts: CollectionEntry<"blog">[]) => {
     .sort(
       (a, b) =>
         Math.floor(
-          new Date(b.data.modDatetime ?? b.data.pubDatetime).getTime() / 1000
+          new Date(
+            b.data.modDatetime && b.data.modDatetime > b.data.pubDatetime
+              ? b.data.modDatetime
+              : b.data.pubDatetime
+          ).getTime() / 1000
         ) -
         Math.floor(
-          new Date(a.data.modDatetime ?? a.data.pubDatetime).getTime() / 1000
+          new Date(
+            a.data.modDatetime && a.data.modDatetime > a.data.pubDatetime
+              ? a.data.modDatetime
+              : a.data.pubDatetime
+          ).getTime() / 1000
         )
     );
 };
