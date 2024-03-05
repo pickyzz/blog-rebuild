cfafc77dcf4da1ff37e1a860eafb80a04aed36e3
Parinya T. <pikcolokung@gmail.com>
2024-03-05 16:32:17 +0700

    style: remove undefined class & change "updated" into badge

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -12,10 +12,12 @@ export default function Datetime({
   pubDatetime,
   modDatetime,
   size = "sm",
-  className,
+  className = "",
 }: Props) {
   return (
-    <div className={`flex items-center space-x-2 opacity-80 ${className}`}>
+    <div
+      className={`flex items-center space-x-2 opacity-80 ${className}`.trim()}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className={`${
@@ -27,11 +29,13 @@ export default function Datetime({
         <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
       </svg>
       {modDatetime && modDatetime > pubDatetime ? (
-        <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+        <span
+          className={`italic ${size === "sm" ? "text-sm" : "text-base"} badge badge-primary badge-md badge-outline`}
+        >
+          UPDATED
         </span>
       ) : (
-        <span className="sr-only">Published:</span>
+        <span className="sr-only">Published</span>
       )}
       <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
         <FormattedDatetime
