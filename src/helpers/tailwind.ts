73ac66e4e7f10538f9996b1a3a4b9c4202fc7cab
Parinya T. <pikcolokung@gmail.com>
2024-04-06 20:46:18 +0700

    remove: unused files

--- src/helpers/tailwind.ts
+++
@@ -1,4 +0,0 @@
-// tw is a template literal that allows prettier-plugin-tailwindcss to sort and format tailwind classes in strings
-// Usage: tw`bg-white p-4 dark:bg-black`
-export const tw = (strings, ...values) =>
-  String.raw({ raw: strings }, ...values);
