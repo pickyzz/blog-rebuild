e500bb69909f8cfd805ab21a79773c52195b7dcf
Parinya T. <pikcolokung@gmail.com>
2024-12-05 16:16:04 +0700

    chore: etc.

--- src/utils/getPostsWithRT.ts
+++ src/utils/getPostsWithRT.ts
@@ -2,16 +2,17 @@ import { slugifyStr } from "./slugify";
 import type { CollectionEntry } from "astro:content";
 
 export const getReadingTime = async () => {
-  const globPosts = import.meta.glob<CollectionEntry<"blog">>(
-    "../content/blog/*{.mdx,.md}",
-    { eager: false }
+  const globPosts = Object.values(
+    import.meta.glob<CollectionEntry<"blog">>("../content/blog/*{.mdx,.md}", {
+      eager: false,
+    })
   );
 
   const mapFrontmatter = new Map<string, number>();
   const globPostsValues = Object.values(globPosts);
   await Promise.all(
     globPostsValues.map(async globPost => {
-      const { frontmatter } = await globPost();
+      const { frontmatter }: any = await globPost();
       mapFrontmatter.set(
         slugifyStr(frontmatter.title),
         frontmatter.readingTime
@@ -26,7 +27,9 @@ const getPostsWithRT = async (posts: CollectionEntry<"blog">[]) => {
   const mapFrontmatter = await getReadingTime();
 
   return posts.map(post => {
-    post.data.readingTime = mapFrontmatter.get(slugifyStr(post.data.title));
+    post.data.readingTime = String(
+      mapFrontmatter.get(slugifyStr(post.data.title))
+    );
     return post;
   });
 };
